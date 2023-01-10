281248b69d207f370b52d0de7e3c0c102f4245dc
Shripada Hebbar <shripada.hebbar@codecraft.co.in>
2023-01-10 16:06:36 +0530

    remove unwanted component files generated during testing

--- src/components/TipButton/TipButton.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { IProps } from './types';
-import styles from './TipButton.module.css';
-
-const TipButton = (props: IProps) => {
-  return <div className={styles.wrapper}>TODO</div>;
-};
-
-export default TipButton;
--- src/components/TipButton/index.ts
+++
@@ -1,2 +0,0 @@
-export { default } from './TipButton';
-export * from './types';
