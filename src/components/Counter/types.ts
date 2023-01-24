27817c17cfc255ebada0604d3a034c9385c74923
Shripada Hebbar <shripada.hebbar@codecraft.co.in>
2023-01-24 10:12:42 +0530

    integrate addon doc storybook addon so that the jsdoc documentation given to props automatically reflects in docs section

--- src/components/Counter/types.ts
+++ src/components/Counter/types.ts
@@ -1,4 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface IProps {
+  /**
+   *  Can pass an initial value to be used for the Counter, by default it will be zero
+   *
+   */
   initialCount?: number;
 }
